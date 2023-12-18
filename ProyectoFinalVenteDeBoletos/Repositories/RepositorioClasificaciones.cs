358633f737287a12ed0ad99e8d1e113415c1fd40
LuisRivera563 <XLS_GS_563@hotmail.com>
2023-12-18 13:51:02 -0600

    Agregue la configuracion para la autentificacion en program.cs

---
+++ ProyectoFinalVenteDeBoletos/Repositories/RepositorioClasificaciones.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalVentaDeBoletos.Models.Entities;
+
+namespace ProyectoFinalVentaDeBoletos.Repositories
+{
+    public class RepositorioClasificaciones: Repository<Clasificacion>
+    {
+        private readonly CinemaventaboletosContext Context;
+        public RepositorioClasificaciones(CinemaventaboletosContext context) : base(context)
+        {
+            this.Context = context;
+        }
+        public override IEnumerable<Clasificacion> GetAll()
+        {
+            return Context.Clasificacion.Include(x => x.Pelicula).OrderBy(x => x.Nombre);
+        }
+        public IEnumerable<Clasificacion> GetClasificaciones()
+        {
+            return GetAll().OrderBy(x => x.Nombre);
+        }
+        public Clasificacion? GetClasificacionByNombre(string nombre)
+        {
+            return Context.Clasificacion.Find(nombre);
+        }
+        public Clasificacion? GetClasificacionById(int id)
+        {
+            return Context.Clasificacion.Find(id);
+        }
+    }
+}
