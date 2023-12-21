ed95125b041d7bb25f02e38b3b3c97ca3ae10110
VICTOR OSVALDO RODRIGUEZ HIDALGO <201G0283@rcarbonifera.tecnm.mx>
2023-12-21 11:57:11 -0600

    horarios

---
+++ ProyectoFinalVenteDeBoletos/Controllers/HorariosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinalVentaDeBoletos.Repositories;
+
+namespace ProyectoFinalVentaDeBoletos.Controllers
+{
+    public class HorariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AgregarHorario() 
+        {
+            return View();
+        }
+        public IActionResult EditarHorario()
+        {
+            return View();
+        }
+        public IActionResult EliminarrHorario()
+        {
+            return View();
+        }
+    }
+}
