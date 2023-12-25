71db875c94b3c9ecedd4f09520e80decc57a8dd5
LuisRivera563 <XLS_GS_563@hotmail.com>
2023-12-25 17:03:16 -0600

    .

--- ProyectoFinalVenteDeBoletos/Program.cs
+++ ProyectoFinalVenteDeBoletos/Program.cs
@@ -6,48 +6,48 @@ using ProyectoFinalVentaDeBoletos.Repositories;
 #region Configuracion General(Builder)
 
     var builder = WebApplication.CreateBuilder(args);
-    #region Conexion a la base de datos
-    
+
+    #region Conexion a la base de datos    
     //DB es la conexion especificada en appsettings.json
     string? Db = builder.Configuration.GetConnectionString("DbConnectionString");
     //Esta es la conexion a la base de datos
-    builder.Services.AddMySql<Sistem21VentaboletosdbContext>(Db, ServerVersion.AutoDetect(Db));
-    
+    builder.Services.AddMySql<Sistem21VentaboletosdbContext>(Db, ServerVersion.AutoDetect(Db));    
     #endregion
 
     //Utilizar MVC
     builder.Services.AddMvc();
+
     #region Repositorios Utilizando AddTransient
 
-    //Para inyectar los repositorios directamente sin dar contexto
-    builder.Services.AddTransient<RepositorioAsientos>();
-    builder.Services.AddTransient<RepositorioBoletos>();
-    builder.Services.AddTransient<RepositorioClasificaciones>();
-    builder.Services.AddTransient<RepositorioHorarios>();
-    builder.Services.AddTransient<RepositorioPeliculas>();
-    builder.Services.AddTransient<RepositorioSalas>();
-    builder.Services.AddTransient<RepositorioUsuarios>();
+        //Para inyectar los repositorios directamente sin dar contexto
+        builder.Services.AddTransient<RepositorioAsientos>();
+        builder.Services.AddTransient<RepositorioBoletos>();
+        builder.Services.AddTransient<RepositorioClasificaciones>();
+        builder.Services.AddTransient<RepositorioHorarios>();
+        builder.Services.AddTransient<RepositorioPeliculas>();
+        builder.Services.AddTransient<RepositorioSalas>();
+        builder.Services.AddTransient<RepositorioUsuarios>();
 
     #endregion
 
-    //#region Autentificacion
-    //builder.Services
-    //    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    //    .AddCookie(x =>
-    //    {
-    //        x.AccessDeniedPath = "/Home/Denied";
-    //        x.LoginPath = "/Home/Login";
-    //        x.LogoutPath = "/Home/Logout";
-    //        x.ExpireTimeSpan = TimeSpan.FromSeconds(50); //TimeSpan.FromMinutes(5); //Tiempo en la que la cookie esta activa.
-    //        x.Cookie.Name = "noticiaCookie";
-    //    });
-    //#endregion
+    #region Autentificacion
+
+    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(x =>
+        {
+            x.AccessDeniedPath = "/Home/Denied";
+            x.LoginPath = "/Home/Login";
+            x.LogoutPath = "/Home/Logout";
+            x.ExpireTimeSpan = TimeSpan.FromSeconds(50); //TimeSpan.FromMinutes(5); //Tiempo en la que la cookie esta activa.
+            x.Cookie.Name = "noticiaCookie";
+        });
+    #endregion
 
 #endregion
 
 #region Configuracion General(App)
 
-    var app = builder.Build();
+var app = builder.Build();
     app.UseStaticFiles();
     app.UseFileServer();
     app.MapDefaultControllerRoute();
