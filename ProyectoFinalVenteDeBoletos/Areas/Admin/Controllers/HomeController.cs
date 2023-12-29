be77a2421caa1d649fc52fa0b14a32ef3e8b163c
AlexanderContreras17 <hatakealexander24@gmail.com>
2023-12-29 17:37:22 -0600

    .

--- ProyectoFinalVenteDeBoletos/Areas/Admin/Controllers/HomeController.cs
+++ ProyectoFinalVenteDeBoletos/Areas/Admin/Controllers/HomeController.cs
@@ -11,11 +11,7 @@ namespace ProyectoFinalVentaDeBoletos.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
-        public Repository<RepositorioUsuarios> UserReposiroty { get;}
-        public HomeController(Repository<RepositorioUsuarios> UserRepository)
-        {
-            this.UserReposiroty = UserRepository;
-        }
+      
         [HttpGet("/Admin")]
         [HttpGet("/Admin/Home/Index")]
         public IActionResult Index()
@@ -23,59 +19,8 @@ namespace ProyectoFinalVentaDeBoletos.Areas.Admin.Controllers
             return View();
         }
 
-        [HttpPost]
-        public IActionResult login()
-        {
-            return View();
-        }
+      
 
-        public IActionResult login(LoginViewModel vm)
-        {
-            if(string.IsNullOrWhiteSpace(vm.Username))
-            {
-                ModelState.AddModelError("", "Username incorrecto");
-            }
-            if (string.IsNullOrWhiteSpace(vm.Contraseña))
-            {
-                ModelState.AddModelError("", "Contraseña incorrecta");
-            }
-
-            if (ModelState.IsValid)
-            {
-                var user = UserReposiroty.GetAll().FirstOrDefault(x => x.CorreoElectronico == vm.Contraseña
-               && x.contraseña == Encriptacion.StringToSHA512(vm.Contraseña));
-                if (user == null)
-                {
-                    ModelState.AddModelError("", "Contraseña o correo electrónico incorrectos");
-                }
-                else
-                {
-                    //logear
-                    List<Claim> claims = new List<Claim>();
-                    claims.Add(new Claim("Id", user.Id.ToString()));
-                    claims.Add(new Claim(ClaimTypes.Name, user.Nombre));
-                    claims.Add(new Claim(ClaimTypes.Role, user.Rol == 1 ? "Administrador" : "Supervisor"));
-
-                    ClaimsIdentity identity = new(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                    HttpContext.SignInAsync(new ClaimsPrincipal(identity));
-
-                    return RedirectToAction("Index", "Home", new { area = "Admin" });
-
-
-                }
-
-            }
-                return View(vm);
-        }
-
-        public IActionResult Logout()
-        {
-            HttpContext.SignOutAsync();
-            return RedirectToAction("Index", "Home");
-        }
-        public IActionResult Denied()
-        {
-          return View();
-        }
+      
     }
 }
