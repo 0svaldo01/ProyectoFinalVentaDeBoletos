99379ec5282475cee56c2f70228702efd7f7b32f
VICTOR OSVALDO RODRIGUEZ HIDALGO <201G0283@rcarbonifera.tecnm.mx>
2023-12-30 22:16:35 -0600

    reverti lo que habia hecho

--- ProyectoFinalVenteDeBoletos/Areas/Admin/Models/Horarios/AgregarHorarioViewModel.cs
+++ ProyectoFinalVenteDeBoletos/Areas/Admin/Models/Horarios/AgregarHorarioViewModel.cs
@@ -4,10 +4,6 @@ namespace ProyectoFinalVentaDeBoletos.Areas.Admin.Models.Horarios
 {
     public class AgregarHorarioViewModel
     {
-        string HoraInicio { get; set; } = null!;
-        string HoraTermino { get; set; } = null!;
-        public int IdSala { get; set; }
-        public IEnumerable<Sala> Salas { get; set; } = null!;
         public int IdPelicula {  get; set; }
         public IEnumerable<Pelicula> Peliculas { get; set; } = null!;
         public int IdHorario { get; set; }
